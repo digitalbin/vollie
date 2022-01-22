334cec464f57090a5a9e4155d6ad35164f298a85
Albin Hagström <albin.hagstrom@nentgroup.com>
2022-01-22 13:36:49 +0100

    he

--- svelte.config.js
+++ svelte.config.js
@@ -2,6 +2,8 @@
 import adapter from '@sveltejs/adapter-static';
 import preprocess from 'svelte-preprocess';
 
+const dev = process.env.NODE_ENV === 'development';
+
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	// Consult https://github.com/sveltejs/svelte-preprocess
@@ -10,7 +12,9 @@ const config = {
 
 	kit: {
 		adapter: adapter(),
-
+		paths: {
+			base: dev ? '' : '/vollie',
+		},
 		// hydrate the <div id="svelte"> element in src/app.html
 		target: '#svelte'
 	}
