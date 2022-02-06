bd4fd33e2b75eef9a180ae11510f2038bf2699e6
Albin Hagström <albin.hagstrom@nentgroup.com>
2022-02-06 11:26:32 +0100

    he

--- vite.config.js
+++ vite.config.js
@@ -38,7 +38,7 @@ export default defineConfig({
                 theme_color: '#ffffff',
                 background_color: '#ffffff',
                 display: 'standalone',
-                start_url: '/',
+                start_url: './',
             },
         }),
     ],
